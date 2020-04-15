847aa28a04e0e49ea1c66d3b8a3b0940c23d7dcb
Johannes Hufnagl <e11918516@student.tuwien.ac.at>
2020-04-15 21:30:35 +0200

    Git Teamtest: Configuration.csv, In.java, ReadDataUtil.java, StdDraw.java, Vector3.java

---
+++ src/Vector3.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+
+// This class represents vectors in a 3D vector space.
+public class Vector3 {
+
+    private double x;
+    private double y;
+    private double z;
+
+    public Vector3() {
+    }
+
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    // Returns the sum of this vector and vector 'v'.
+    public Vector3 plus(Vector3 v) {
+        Vector3 result = new Vector3();
+        result.x = this.x + v.x;
+        result.y = this.y + v.y;
+        result.z = this.z + v.z;
+        return result;
+    }
+
+    // Returns the product of this vector and 'd'.
+    public Vector3 times(double d) {
+        Vector3 result = new Vector3();
+        result.x = this.x * d;
+        result.y = this.y * d;
+        result.z = this.z * d;
+        return result;
+    }
+
+    // Returns the sum of this vector and -1*v.
+    public Vector3 minus(Vector3 v) {
+        Vector3 result = new Vector3();
+        result.x = this.x - v.x;
+        result.y = this.y - v.y;
+        result.z = this.z - v.z;
+        return result;
+    }
+
+    // Returns the Euclidean distance of this vector
+    // to the specified vector 'v'.
+    public double distanceTo(Vector3 v) {
+        double dX = this.x - v.x;
+        double dY = this.y - v.y;
+        double dZ = this.z - v.z;
+        return Math.sqrt(dX * dX + dY * dY + dZ * dZ);
+    }
+
+    // Returns the length (norm) of this vector.
+    public double length() {
+        return distanceTo(new Vector3());
+    }
+
+    // Normalizes this vector: changes the length of this vector such that it becomes one.
+    // The direction and orientation of the vector is not affected.
+    public void normalize() {
+        double length = length();
+        this.x /= length;
+        this.y /= length;
+        this.z /= length;
+    }
+
+    // Draws a filled circle with the center at (x,y) coordinates of this vector
+    // in the existing StdDraw canvas. The z-coordinate is not used.
+    public void drawAsDot(double radius, Color color) {
+        StdDraw.setPenColor(color);
+        StdDraw.filledCircle(this.x, this.y, radius);
+    }
+
+    // Returns the coordinates of this vector in brackets as a string
+    // in the form "[x,y,z]", e.g., "[1.48E11,0.0,0.0]".
+    public String toString() {
+        return "[" + this.x + "," + this.y + "," + this.z + "]";
+    }
+
+    // Prints the coordinates of this vector in brackets to the console (without newline)
+    // in the form [x,y,z], e.g.,
+    // [1.48E11,0.0,0.0]
+    public void print() {
+        System.out.print(toString());
+    }
+
+}
+
